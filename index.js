48475a7f459a4141a0e94f086411b61c9749cc6e
jawadGitNext <jawadGitNext2021@gmail.com>
2021-10-01 10:31:58 +0300

    env

--- index.js
+++ index.js
@@ -29,22 +29,37 @@ app.get("/", (req, res) => {
   res.send("running")
 })
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   const email = req.body.form_email
   console.log("jjjjjj")
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        console.log("Server is ready to take our messages")
+        resolve(success)
+      }
+    })
+  })
+
   var mailOptions = {
     from: process.env.MY_EMAIL,
     to: email,
     subject: "Sending Email using Node.js",
     text: `${email} has just subscribed`,
   }
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log("Email sent: " + info.response)
-    }
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log("Email sent: " + info.response)
+      }
+    })
   })
   console.log("jjjjjjjjjjjjjj")
   res.json({ success: "Thanks for the subscribe" })
